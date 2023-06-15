e3a84385a77daa2b4cd6b799b78ec4bfbc22f0bf
Takashi Yamamiya <tak@metatoys.org>
2023-06-16 00:37:47 +0900

    Toggle camera and image

--- app/page.tsx
+++ app/page.tsx
@@ -2,32 +2,41 @@
 
 import React, { useRef, useEffect, useState } from 'react';
 
+function startCamera(videoRef: React.RefObject<HTMLVideoElement>) {
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(function (stream) {
+        // Link the video source to the video element
+        if (videoRef != null && videoRef.current != null) {
+          videoRef.current.srcObject = stream;
+        }
+      })
+      .catch(function (err) {
+        console.log("An error occurred: " + err);
+      });
+  }
+}
+
 export default function Home() {
   const videoRef = useRef<HTMLVideoElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [imageUrl, setImageUrl] = useState<string>("");
+  const captureLabel = imageUrl ? "Retake" : "Capture";
 
   useEffect(() => {
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia({ video: true })
-        .then(function (stream) {
-          // Link the video source to the video element
-          if (videoRef != null && videoRef.current != null) {
-            videoRef.current.srcObject = stream;
-          }
-        })
-        .catch(function (err) {
-          console.log("An error occurred: " + err);
-        });
-    }
+    startCamera(videoRef);
   }, []);
 
   const captureImage = () => {
     const canvas = canvasRef.current;
     const video = videoRef.current;
 
+    if (!video || !canvas) {
+      return;
+    }
 
-    if (video && canvas !== null) {
+    if (captureLabel === "Capture") {
+
       // Set the canvas dimensions to match the video
       canvas.width = video.videoWidth;
       canvas.height = video.videoHeight;
@@ -37,9 +46,8 @@ export default function Home() {
       // Get the image data URL from the canvas
       const capturedImageUrl = canvas.toDataURL('image/png');
       // Do something with the image (imageUrl)
-      console.log(capturedImageUrl);
+      //console.log(capturedImageUrl);
       setImageUrl(capturedImageUrl);
-
 
       // Stop all video tracks
       if (video.srcObject != null) {
@@ -49,16 +57,19 @@ export default function Home() {
 
       // Remove srcObject from the video
       video.srcObject = null;
+
+    } else {
+      setImageUrl("");
+      startCamera(videoRef);
     }
   };
 
   return (
     <div>
-      {!imageUrl && <video ref={videoRef} autoPlay={true} /> }
-      {imageUrl && <img src={imageUrl} alt="Captured" />}
+      <video ref={videoRef} autoPlay={true} style={{ display: imageUrl ? "none" : "block" }} />
+      <img src={imageUrl} alt="Captured" style={{ display: imageUrl ? "block" : "none" }} />
       <canvas ref={canvasRef} style={{ display: 'none' }} />
-      <button onClick={captureImage}>Capture</button>
-
+      <button onClick={captureImage}>{captureLabel}</button>
     </div>
   );
 }
