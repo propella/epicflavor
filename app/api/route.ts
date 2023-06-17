349d31627dc13b9f9b24da562f93827997ca187f
Takashi Yamamiya <tak@metatoys.org>
2023-06-17 12:54:52 +0900

    Google vision api

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from 'next/server'
+import vision from '@google-cloud/vision';
 
 export async function GET(request: Request) {
     const response = {
@@ -9,16 +10,43 @@ export async function GET(request: Request) {
 }
 
 /**
+ * By using Google Vision API, it detects label from the image url
+ */
+async function getLabel(imageUrl: string): Promise<string> {
+    // Imports the Google Cloud client library
+    // const vision = require('@google-cloud/vision');
+
+    console.log("getLabel...")
+    const base64Image = imageUrl.replace(/^data:image\/(png|jpg);base64,/, "");
+    const request = {
+        image: {
+          content: base64Image,
+        },
+      };
+
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
+    const [result] = await client.labelDetection(request);
+    // console.log({result});
+    const labels = result.labelAnnotations;
+    console.log('Labels:');
+    // labels.forEach(label => console.log(label.description));
+    const description = labels?.map((label) => label.description).join(", ")
+    return description || "";
+}
+
+/**
  * POST method receives a image URL and returns a JSON with the string description
  * @param request
  */
 export async function POST(request: Request) {
     const { imageUrl } = await request.json();
     const text = `happy face: ${new Date()}`;
+    const description = await getLabel(imageUrl);
 
     const response = {
-        "result": text,
+        "result": description,
     };
-    console.log({ imageUrl });
+    // console.log({ imageUrl });
     return NextResponse.json(response);
 }
